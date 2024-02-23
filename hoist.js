a00804d61dfeddbbb654d2d2b8e396d947f485d1
KSidharth <sidharthraok@gmail.com>
2024-02-23 12:45:27 +0530

    Commiting hoist

---
+++ hoist.js
@@ -0,0 +1,21 @@
+// x = 5;
+
+// elem = document.getElementById("demo"); 
+// elem.innerHTML = x;           
+
+// var x; 
+// function Hoist() {
+//     a = 1;
+//     let b = 2;
+//     }
+//     Hoist();
+    
+//     console.log(a);
+//     console.log(b); 
+
+function Hoist() {
+    let name = 'Sidharth';
+    console.log(name); 
+    }
+Hoist();
+        
